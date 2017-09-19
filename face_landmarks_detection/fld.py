41b3c6522f8997ff5c144b56e3d8f4de6fc6946c
Tianxin Tse <xietx1995@gmail.com>
2017-09-19 11:07:52 +0800

    add function to get face landmarks

--- face_landmarks_detection/fld.py
+++ face_landmarks_detection/fld.py
@@ -11,16 +11,46 @@ TEST_IMG = PARENT_PATH + '/imgs/ldh.jpg'
 PREDICTOR_MODEL = PARENT_PATH + '/models/predic.dat'
 
 
+def get_landmarks(img_file=TEST_IMG, predic_path=PREDICTOR_MODEL):
+    """
+    Use predict_model to detect face in img, and return the landmarks.
+    Default values for parameters can be changed.
+
+    :param img: A jpg image that contains a face.
+    :param predict_model: The model for predicting face.
+    :return: landmarks
+    """
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(PREDICTOR_MODEL)
+    img = io.imread(img_file)
+
+    # Ask the detector to find the bounding boxes of each face. The 1 in the
+    # second argument indicates that we should upsample the image 1 time. This
+    # will make everything bigger and allow us to detect more faces.
+    dets = detector(img, 1)
+
+    # we just need one face
+    if len(dets) != 1:
+        print("Error:\n"
+              "\tMultiple faces modeling not supported.\n"
+              "\tPlease specify an image with exactly one face.\n")
+        exit()
+
+    shape = predictor(img, dets[0])
+
+    return shape
+
+
 def local_test(img_file=TEST_IMG, predic_path=PREDICTOR_MODEL):
     """
-    Use predict_model to detect face in img.
+    Use predict_model to detect face in img, and show the result in a window.
     Default values for parameters can be changed.
 
     :param img: A jpg image that contains a face.
     :param predict_model: The model for predicting face.
     :return: None
     """
-    print(PARENT_PATH)
+    # print(PARENT_PATH)
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(PREDICTOR_MODEL)
     img = io.imread(img_file)
